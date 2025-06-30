30b8194e5060efba42f87a116af8087be8522fa4
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2025-06-30 16:14:05 +0530

    Update Login_Function.java

--- src/test/java/selenium_jenkins/Login_Function.java
+++ src/test/java/selenium_jenkins/Login_Function.java
@@ -34,18 +34,20 @@ public void setUp() throws IOException {
 }
 	@Test(priority = 1)
 	public  void login() throws InterruptedException, IOException {
-		Properties properties=new Properties();
+		/*Properties properties=new Properties();
 		FileInputStream file = new FileInputStream("/src/test/java/selenium_jenkins/configurationProperties");
 
-		properties.load(file);
+		properties.load(file);*/
 		
 		driver.get("https://www.amazon.in/");
 		driver.findElement(By.xpath("//a[@href=\"https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0\"]")).click();
 		WebElement username=driver.findElement(By.id("ap_email_login"));
-		username.sendKeys(properties.getProperty("username"));
+		//username.sendKeys(properties.getProperty("username"));
+		username.sendKeys("nandhiniravi1402@gmail.com");
 		driver.findElement(By.xpath("//input[@class=\"a-button-input\"]")).click();
 		WebElement password=driver.findElement(By.id("ap_password"));
-		password.sendKeys(properties.getProperty("password"));
+		//password.sendKeys(properties.getProperty("password"));
+		password.sendKeys("Nandhu@01");
 		driver.findElement(By.id("signInSubmit")).click();
 		Thread.sleep(3000);
 	}
