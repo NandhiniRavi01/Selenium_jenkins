4e8122c612021da35bf75ae7d28711b695eab9f7
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2025-07-01 15:04:45 +0530

    Update Login_Function.java

--- src/test/java/selenium_jenkins/Login_Function.java
+++ src/test/java/selenium_jenkins/Login_Function.java
@@ -48,39 +48,48 @@ public void login() throws InterruptedException {
 		driver.findElement(By.id("signInSubmit")).click();
 		Thread.sleep(3000);
 }
-   /* @Test(priority = 2)
-    public void Searchelement() {
-        WebElement search = wait.until(ExpectedConditions.elementToBeClickable(By.id("twotabsearchtextbox")));
-        search.sendKeys("electric cycle");
-        driver.findElement(By.id("nav-search-submit-button")).click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(
-            By.xpath("//span[contains(text(), 'EMotorad X2 Unisex Mountain Electric Cycle')]"))).click();
-
-        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(tabs.get(1));
-    }
-
-    @Test(priority = 3)
-    public void AddCart() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='a-button a-button-dropdown']"))).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("quantity_25"))).click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-button"))).click();
-    }
-
-    @Test(priority = 4)
-    public void GoCart() {
-        wait.until(ExpectedConditions.elementToBeClickable(
-            By.xpath("//a[contains(@href, '/cart') and @class='a-button-text']"))).click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.name("proceedToRetailCheckout"))).click();
-    }
-
+   @Test(priority = 2)
+		public  void Searchelement() throws InterruptedException {
+			
+		WebElement Search=driver.findElement(By.id("twotabsearchtextbox"));
+		Search.sendKeys("electric cycle");
+		driver.findElement(By.id("nav-search-submit-button")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[contains(text(), 'EMotorad X2 Unisex Mountain Electric Cycle')]")).click();
+		
+		 ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+		    driver.switchTo().window(tabs.get(1)); // Switch to product page tab
+		    Thread.sleep(3000);
+		
+	}
+		@Test(priority = 3)
+		public  void AddCart() throws InterruptedException {
+			
+			
+			driver.findElement(By.xpath("//span[@class=\"a-button a-button-dropdown\"]")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.id("quantity_25")).click();
+			Thread.sleep(2000);
+		driver.findElement(By.id("add-to-cart-button")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[contains(text(), 'EMotorad X2 Unisex Mountain Electric Cycle')]")).click();
+		Thread.sleep(3000);
+		
+		
+		
+	}
+		@Test(priority = 4)
+		public  void GoCart() throws InterruptedException {
+			
+			driver.findElement(By.xpath("//a[contains(@href, \"/cart\") and @class=\"a-button-text\"]")).click();
+			Thread.sleep(3000);
+			driver.findElement(By.xpath("//input[@name=\"proceedToRetailCheckout\"]")).click();
+			Thread.sleep(3000);
+		}
     @AfterClass
     public void CloseWebsite() {
         if (driver != null) {
             driver.quit();
         }
-    }*/
+    }
 }
